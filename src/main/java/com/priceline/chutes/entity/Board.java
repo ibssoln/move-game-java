85159c9e00aac2a1b80838251135381465c0d4b2
ibssoln <itbisys.system@gmail.com>
2024-02-14 11:17:21 -0500

    Adds constant variables

--- src/main/java/com/priceline/chutes/entity/Board.java
+++ src/main/java/com/priceline/chutes/entity/Board.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static com.priceline.chutes.constant.GameConstants.SPECIAL_SQUARES;
+import static com.priceline.chutes.constant.GameConstants.SQUARES;
 import static java.util.Map.entry;
 
 public class Board {
@@ -15,47 +17,21 @@ public class Board {
     //NOTE: Created an 'entity' package to better organize the classes.
     // The package 'entity' archives all supporting entity classes, while the main class 'Game' remains at the root directory.
 
-    //NOTE: To achieve a better 'encapsulation' of properties in the classes, explicitly set the class 'properties'
-    // to be 'private', and added missing accessors (getters and setters) for those properties
-    // so that the properties can be accessed by other object via the accessor methods.
+    //NOTE: Moved 'specialSquares' in the Board class to the constant class (and set it to be 'public static final'),
+    // since this data is fixed and final for the given business rules. Also, refactored the property name to use only
+    // the capital letters as it's a constant variable.
 
-    private Map<Integer, BoardSquare> specialSquares = Map.ofEntries(
-            entry(1, new BoardSquare(false, true, 37)),
-            entry(4, new BoardSquare(false, true, 10)),
-            entry(9, new BoardSquare(false, true, 22)),
-            entry(16, new BoardSquare(true, false, 10)),
-            entry(21, new BoardSquare(false, true, 21)),
-            entry(28, new BoardSquare(false, true, 56)),
-            entry(36, new BoardSquare(false, true, 8)),
-            entry(47, new BoardSquare(true, false, 21)),
-            entry(49, new BoardSquare(true, false, 38)),
-            entry(51, new BoardSquare(false, true, 16)),
-            entry(56, new BoardSquare(true, false, 3)),
-            entry(62, new BoardSquare(true, false, 43)),
-            entry(64, new BoardSquare(true, false, 4)),
-            entry(71, new BoardSquare(false, true, 20)),
-            entry(80, new BoardSquare(false, true, 20)),
-            entry(87, new BoardSquare(true, false, 63)),
-            entry (93, new BoardSquare(true, false, 20)),
-            entry(95, new BoardSquare(true, false, 20)),
-            entry(98, new BoardSquare(true, false, 20))
-    );
-
-    private List<BoardSquare> squares;
-
-    //NOTE: In each class, created a base constructor and set the modifier to be 'public'
-    // so that an external class can instantiate this target class.
+    //NOTE: set the modifier to be 'public' so that an external class can instantiate this target class.
     public Board() {
-        squares = java.util.stream.IntStream.rangeClosed(1, 100)
-                .mapToObj(i -> Optional
-                        .ofNullable(specialSquares.get(i))
-                        .orElseGet(BoardSquare::new))
-                .collect(Collectors.toList());
+        super();
+        //NOTE: Moved 'squares' in the Board class to the constant class (and set it to be 'public static final'),
+        //  since this data is fixed and final for the given business rules.
+        //  Also, refactored the property name to use only the capital letters as it's a constant variable.
     }
 
     //NOTE: changed the modifier scope to 'public'.
     public BoardSquare getSquareAtPosition(int i){
-        return squares.get(i-1);
+        return SQUARES.get(i-1);
     }
 
     //NOTE: getters and setters of class properties can be useful for the extensibility and scalability of the application.
@@ -65,7 +41,7 @@ public class Board {
     //NOTE: In order to maintain 'immutability', when we send a list to an external entity,
     // we need to wrap the existing list with a new 'ArrayList' once again and send a 'copy' of it, not to send the original list as a reference.
     public List<BoardSquare> getSquares() {
-        return new ArrayList<>(squares); //NOTE: for immutability, we need to return a new copy of the existing list object, not the list itself.
+        return new ArrayList<>(SQUARES); //NOTE: for immutability, we need to return a new copy of the existing list object, not the list itself.
     }
 
     //NOTE: We will not have/allow a 'setSquares()' method, because we want to lock the squares once it's made.
